3727166330a1d8c7f34c66454145f6c4c8047080
oesl1902@gmail.com <oesl1902@gmail.com>
2022-06-21 19:02:07 -0500

    Agrega los ejercicios 1 y 2 del curso

---
+++ Oscar Sanchez/movies/src/main/java/com/oeslapps/movies/entities/Genre.java
@@ -0,0 +1,23 @@
+package com.oeslapps.movies.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Genre {
+    private String id;
+    private String name;
+
+    @Override
+    public String toString() {
+        return "Genre{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
